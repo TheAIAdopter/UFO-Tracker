6fc451c09c2987056c79373941d1d60f87e9b72f
TheAIAdopter <130402077+TheAIAdopter@users.noreply.github.com>
2024-12-26 19:21:42 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+```javascript
+const CACHE_NAME = 'ufo-tracker-v1';
+const urlsToCache = [
+  '/UFO-Tracker/',
+  '/UFO-Tracker/index.html',
+  '/UFO-Tracker/manifest.json',
+  '/UFO-Tracker/icon-192.png',
+  '/UFO-Tracker/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+```
